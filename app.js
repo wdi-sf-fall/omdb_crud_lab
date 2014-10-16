bb29da96cbac5c45052b012aed56a9983cd1647d
Tim Licata <tim@timothylicata.com>
2014-10-16 12:59:47 -0700

    code we added in the review on thursday morning

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var request = require("request");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 var app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
 
 app.get('/', function(req, res){
   res.render('index.ejs');
@@ -31,6 +35,27 @@ app.get('/details/:id', function(req, res){
       res.render("details.ejs", {movieDetails: obj});
     }
   });
+});
+
+var watchLater = [];
+app.get('/watchlater', function (req, res) {
+  res.render('watchlater.ejs', {watchLater: watchLater});
+});
+app.post('/watchlater', function (req, res) {
+  var id = req.body.id;
+  var title = req.body.title;
+  watchLater.push({id:id, title:title});
+  res.redirect("/watchlater");
+});
+app.delete('/watchlater', function (req, res) {
+  // remove a movie from the watchLater list
+  var id = req.body.id;
+  for (var i = 0; i < watchLater.length; i++) {
+    if (watchLater[i].id === id) {
+      watchLater.splice(i, 1);
+    }
+  }
+  res.redirect("/watchlater");
 });
 
 app.listen(3000);
